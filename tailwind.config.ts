5a1f4b064910375318c867bd6ef6e8034e70d1bf
Konrad Nowakowski <konrad.x92@gmail.com>
2024-02-08 12:26:51 +0000

    feat(app): add tailwind brand theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary': '#6CBE45',
+        'on-primary': '#FFFFFF',
+        'surface': '#2A2728',
+        'on-surface': '#FFFFFF',
+        'surface-variant': '#443F41',
+        'on-surface-variant': '#FFFFFF',
+        'surface-light': '#F5F5F5',
+        'on-surface-light': '#2A2728',
+        'background': '#FFFFFF',
+        'on-background': '#2A2728',
+      },
     },
     screens: {
       sm: '576px',
