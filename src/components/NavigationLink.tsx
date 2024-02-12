2870f7cba2b0700828650f5e4150a1e97972cfc2
Konrad Nowakowski <konrad.x92@gmail.com>
2024-02-12 18:52:47 +0000

    feat(components): make nav link active based on pathname

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -8,13 +8,17 @@ export type NavigationLinkProps = PropsWithChildren<LinkProps>
 
 const NavigationLink = ({ children, ...props }: NavigationLinkProps) => {
   const pathname = usePathname()
-  const isActive = pathname === props.href
+  const isActive = resolveIsActive(String(props.href), pathname)
 
   return (
     <Link className={`group mx-4 ${isActive ? 'is-active' : ''}`} {...props}>
-      <span className="group-[.is-active]:text-primary font-semibold">{children}</span>
+      <span className="font-semibold group-[.is-active]:text-primary">{children}</span>
     </Link>
   )
 }
 
 export default NavigationLink
+
+const resolveIsActive = (linkUri: string, pathname: string) => {
+  return linkUri.length < 2 ? linkUri === pathname : pathname.startsWith(linkUri)
+}
