694ee56567e17eb84ef876aa905183e226464992
Konrad Nowakowski <konrad.x92@gmail.com>
2024-02-19 19:43:15 +0000

    feat(components): add toggle button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,19 +2,24 @@ import { ForwardedRef, ButtonHTMLAttributes, forwardRef } from 'react'
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: 'solid' | 'outline' | 'link'
+  isActive?: boolean
 }
 
 export type ButtonRef = ForwardedRef<HTMLButtonElement>
 
-const Button = ({ variant = 'solid', ...props }: ButtonProps, ref: ButtonRef) => (
-  <button ref={ref} className={`${className.base} ${className[variant]}`} {...props} />
+const Button = ({ variant = 'solid', isActive, ...props }: ButtonProps, ref: ButtonRef) => (
+  <button ref={ref} className={selectClassName(variant, isActive)} {...props} />
 )
 
 export default forwardRef(Button)
+
+const selectClassName = (variant: 'solid' | 'outline' | 'link', isActive = true) =>
+  `${className.base} ${className[isActive ? variant : 'inactive']}`
 
 const className = {
   base: 'rounded-full border-2 px-5 py-1 text-lg font-semibold',
   solid: 'border-primary bg-primary text-on-primary hover:opacity-85',
   outline: 'border-primary text-primary hover:opacity-85',
   link: 'border-transparent text-primary hover:opacity-85',
+  inactive: 'border-gray-300 bg-gray-300 text-on-surface hover:opacity-85',
 }
