7715b77c6632d836c1b785781f30a1c38ca3900c
Konrad Nowakowski <konrad.x92@gmail.com>
2024-02-20 11:43:40 +0000

    feat(portfolio): add header styling

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,9 +2,11 @@ import { PortfolioSectionFilter } from '@/features/portfolio'
 
 const PortfolioPage = () => (
   <section className="flex flex-col">
-    <header className="flex flex-col items-center">
-      <h1 className="text-5xl">My Portfolio</h1>
-      <p>
+    <header className="flex flex-col items-center py-[96px]">
+      <h1 className="mb-4 text-5xl font-bold">
+        My <span className="text-primary">Portfolio</span>
+      </h1>
+      <p className="text-center">
         Explore my diverse graphic design portfolio – creativity in every pixel.
         <br />
         Get inspired or let's collaborate!
