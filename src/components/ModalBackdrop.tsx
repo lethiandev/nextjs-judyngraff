6251369ad1ab2e03d180966dc0585ad591a4c195
Konrad Nowakowski <konrad.x92@gmail.com>
2024-03-07 11:21:35 +0000

    feat(components): remove unused components

--- src/components/ModalBackdrop.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-const ModalBackdrop = ({ children }: PropsWithChildren) => (
-  <div className="absolute left-0 top-0 h-full w-full bg-background p-8 opacity-50">{children}</div>
-)
-
-export default ModalBackdrop
--- src/components/PanelDivider.tsx
+++
@@ -1,3 +0,0 @@
-const PanelDivider = () => <hr className="my-8 border-current opacity-10" />
-
-export default PanelDivider
