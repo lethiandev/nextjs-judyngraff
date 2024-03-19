639fe3596b00bb48a43b247488310454be5db965
Konrad Nowakowski <konrad.x92@gmail.com>
2024-03-19 12:45:00 +0000

    build(npm): add mdx dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const withMDX = require('@next/mdx')()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
