b9a16c3e133f206909cdfd344ca49ecc405d54c8
Konrad Nowakowski <konrad.x92@gmail.com>
2024-03-20 13:02:57 +0000

    fix(partials): make footer contact form more rwd

--- src/partials/FooterContactForm.tsx
+++ src/partials/FooterContactForm.tsx
@@ -1,41 +1,30 @@
 import Button from '@/components/Button'
 
 const FooterContactForm = () => (
-  <form className="mx-auto grid max-w-[860px] gap-8 md:grid-cols-2">
-    <input
-      className="rounded-[2rem] bg-background px-6 py-4 placeholder-gray-200 shadow-lg shadow-gray-100 outline-primary"
-      placeholder="Name"
-      type="text"
-      name="name"
-    />
-    <input
-      className="rounded-[2rem] bg-background px-6 py-4 placeholder-gray-200 shadow-lg shadow-gray-100 outline-primary"
-      placeholder="Email"
-      type="email"
-      name="email"
-    />
-    <input
-      className="rounded-[2rem] bg-background px-6 py-4 placeholder-gray-200 shadow-lg shadow-gray-100 outline-primary"
-      placeholder="Phone"
-      type="tel"
-      name="phone"
-    />
-    <input
-      className="rounded-[2rem] bg-background px-6 py-4 placeholder-gray-200 shadow-lg shadow-gray-100 outline-primary"
-      placeholder="Budget"
-      type="text"
-      name="budget"
-    />
-    <textarea
-      className="col-span-2 mb-8 rounded-[2rem] bg-background px-6 py-4 placeholder-gray-200 shadow-lg shadow-gray-100 outline-primary"
-      placeholder="Message"
-      name="message"
-      rows={8}
-    />
-    <div className="col-span-2 flex justify-center">
+  <form className="mx-auto max-w-[860px]">
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+      <Input placeholder="Name" name="name" type="text" />
+      <Input placeholder="Email" name="email" type="email" />
+      <Input placeholder="Phone" name="phone" type="tel" />
+      <Input placeholder="Budget" name="budget" type="text" />
+      <textarea
+        className="mb-8 rounded-[2rem] bg-background px-6 py-4 placeholder-gray-200 shadow-lg shadow-gray-100 outline-primary md:col-span-2"
+        placeholder="Message"
+        name="message"
+        rows={8}
+      />
+    </div>
+    <div className="flex justify-center md:col-span-2">
       <Button>Send Email &rarr;</Button>
     </div>
   </form>
 )
 
 export default FooterContactForm
+
+const Input = (props: JSX.IntrinsicElements['input']) => (
+  <input
+    className="w-full max-w-full rounded-[2rem] bg-background px-6 py-4 placeholder-gray-200 shadow-lg shadow-gray-100 outline-primary"
+    {...props}
+  />
+)
