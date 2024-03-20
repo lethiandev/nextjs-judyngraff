2e39d52aed22662a090f0d94c7a46340405a166d
Konrad Nowakowski <konrad.x92@gmail.com>
2024-03-20 12:45:25 +0000

    feat(experience): remove redundant experience feature directory

--- src/features/experience/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './ExperienceTimeline'
-export * from './experience'
--- src/features/experience/utils.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(date: Date | string | undefined, fallback = 'Present') {
-  return date ? formatDateToString(date) : fallback
-}
-
-export function formatDateToString(date: Date | string) {
-  return new Date(date).toLocaleDateString('en-GB', { year: 'numeric', month: 'long' })
-}
